7490f664fcc1192dfab61390df563404efdcf56a
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2024-06-03 15:17:37 +0530

    Create LatestDropDownDemoSelenium.java

---
+++ LatestDropDownDemoSelenium.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class M=LatestDropDownDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("Webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/#");
+		
+		driver.findElement(By.cssSelector("#ctl00_mainContent_rbtnl_Trip_1")).click();
+		
+		Thread.sleep(2000L);
+		
+		for(int i=1;i<3;i++) {
+			driver.findElement(By.cssSelector("#ctl00_mainContent_chk_SeniorCitizenDiscount")).click();
+		}
+		Thread.sleep(2000L);
+
+	
+	for(int i=1;i>2;i++) {
+		driver.findElement(By.cssSelector("#ctl00_mainContent_chk_SeniorCitizenDiscount")).click();
+		
+	}
+	Thread.sleep(2000L);
+	
+	for(int i=0;i<1;i++) {
+		driver.findElement(By.cssSelector("#autosuggest")).sendKeys("New Zealand");
+		
+	}
+Thread.sleep(2000L);
+
+driver.getCurrentUrl();
+System.out.println("The Current URL is:" + driver.getCurrentUrl());
+driver.close();
+}
+}
