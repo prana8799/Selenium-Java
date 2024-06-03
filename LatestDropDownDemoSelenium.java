e6c10053057257e57529a1f8ad692dad31f320bb
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2024-06-03 15:25:02 +0530

    Update LatestDropDownDemoSelenium.java

--- LatestDropDownDemoSelenium.java
+++ LatestDropDownDemoSelenium.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class M=LatestDropDownDemo {
+public class LatestDropDownDemo {
 
 	public static void main(String[] args) throws InterruptedException {
 		
