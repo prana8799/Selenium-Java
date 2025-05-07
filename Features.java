b6e855e107f5d38100d56423bb9a5ec9295b94fa
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2025-05-07 14:30:07 +0530

    Update Features.java

--- Features.java
+++ Features.java
@@ -15,7 +15,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Features {
+public class FeaturesLive {
 
 	
 		
@@ -47,7 +47,7 @@ public class Features {
 
 	        Actions ac = new Actions(driver);
 	        ac.moveToElement(practiceid)
-	          .sendKeys("136109").pause(2000)
+	          .sendKeys("349301").pause(2000)
 	          .keyDown(Keys.ARROW_DOWN)  
 	          .sendKeys(Keys.ENTER)      
 	          .perform();
@@ -72,59 +72,59 @@ for(String handle : allWindowHandles) {
 
 WebElement invpat=driver.findElement(By.xpath("//*[@name='invite_patient_flag' and @value='Y']"));
 
-if(invpat.isEnabled()) {
+if(invpat.isSelected()) {
 	System.out.println("\n \n Single Invitation enabled");
 }else
 	System.out.println("\n \n Single Invitation not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='reputation_management_flag' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='reputation_management_flag' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Survey enabled");
 }else
 	System.out.println("\n \n Survey not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='enhanced_scheduling' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='enhanced_scheduling' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Enhanced Scheduling enabled");
 }else
 	System.out.println("\n \n Enhanced Scheduling not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='self_scheduling_flag' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='self_scheduling_flag' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Old Scheduling enabled");
 }else
 	System.out.println("\n \n Old Scheduling not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='two_way_communication_for_scheduled_patients' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='two_way_communication_for_scheduled_patients' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Two way communication version 1 enabled");
 }else
 	System.out.println("\n \n Two way communication version 1 not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='show_two_way_chats_only_for_selected_practice' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='show_two_way_chats_only_for_selected_practice' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Show two way chats for only selected patients enabled");
 }else
-	System.out.println("\\n \\n Show two way chats for only selected patients not enabled");
+	System.out.println("\n \n Show two way chats for only selected patients not enabled");
 
-if(driver.findElement(By.xpath("//*[@name='dashboard_two_way_communication_v2' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='dashboard_two_way_communication_v2' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Two way communication version 2 enabled");
 }else
 	System.out.println("\n \n Two way communication version 2 not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='yosichat_bulk_messaging' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='yosichat_bulk_messaging' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n Yosi broadcast messaging enabled");
 }else
 	System.out.println("\n \n Yosi broadcast messaging not enabled");
 
 
-if(driver.findElement(By.xpath("//*[@name='hpi_new_functionality_flag' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='hpi_new_functionality_flag' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n HPI ROS flag enabled");
 }else
 	System.out.println("\n \n HPI ROS flag not enabled");
 
-if(driver.findElement(By.xpath("//*[@name='ondemand_form' and @value='Y']")).isEnabled()) {
+if(driver.findElement(By.xpath("//*[@name='ondemand_form' and @value='Y']")).isSelected()) {
 	System.out.println("\n \n On demand form enabled");
 }else
 	System.out.println("\n \n On demand form not displayed");
